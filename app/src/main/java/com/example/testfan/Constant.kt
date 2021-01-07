1fd6bb2e22d883fb5642a7e845e5c830df52d0e6
ardion <ardionm97@gmail.com>
2021-01-08 03:59:30 +0800

    first commit

--- app/src/main/java/com/example/testfan/Constant.kt
+++ app/src/main/java/com/example/testfan/Constant.kt
@@ -2,16 +2,6 @@ package com.example.testfan
 
 class Constant {
     companion object{
-        val pref_is_login="pref_is_login"
-        val pref_is_form="pref_is_form"
-        val pref_is_Username="pref_is_Username"
-        val pref_is_Pasword="pref_is_Pasword"
-        val pref_is_regUsername="pref_is_regUsername"
-        val pref_is_regPasword="pref_is_regPasword"
-        val pref_is_signup="pref_is_Signup"
-        val PREF_TOKEN = "PREF_TOKEN"
-        const val EXTRA_EMAIL = "EXTRA_EMAIL"
-        const val EXTRA_PASSWORD = "EXTRA_PASSWORD"
         const val NAME_USER = "NAME_USER"
         const val EMAIL = "EMAIL"
         const val PASSWORD = "PASSWORD"
